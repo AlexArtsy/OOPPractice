e2e795d6ca904b9d16b77f9e36dc1cba1cff0471
Арцыбашев Александр (Artsybashev_AY) <Artsybashev_AY@directum.ru>
2025-07-01 15:23:45 +0400

    Коммит

---
+++ DocumentManager/Document/ContentFormat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManager
+{
+    internal enum ContentFormat
+    {
+        Txt,
+        Doc,
+        Rtf,
+        Fm1
+    }
+}
---
+++ DocumentManager/Document/Document.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManager
+{
+    internal class Document : IDocument
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ContentFormat Format { get; set; }
+        public IEnumerable<byte> Content { get; set; } = [];
+
+        public Document(string name, ContentFormat format)
+        {
+            this.Id = new Guid();
+            this.Name = name;
+            this.Format = format;
+        }
+    }
+}
