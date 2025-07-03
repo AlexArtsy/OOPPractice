b6d30962b63ab8e5bcfaff4c5bbb7161932b5c20
Арцыбашев Александр (Artsybashev_AY) <Artsybashev_AY@directum.ru>
2025-07-03 13:48:27 +0400

    Реализован DI - контейнер

---
+++ DocumentManager/DependencyContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DocumentManager
+{
+    internal class DependencyContainer
+    {
+        public static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            //  Если вдруг не нужно кешировать
+            //services.AddSingleton<IStorageProvider, StorageProvider.StorageProvider>();
+
+            services.AddSingleton<IStorageProvider, StorageProvider.CachedStorageProvider>();
+            services.AddScoped<IEditorProvider, EditorProvider.EditorProvider>();
+
+            services.AddTransient<App>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
